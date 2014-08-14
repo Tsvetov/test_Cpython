b7609ea9be8780a4a08db46f3fad496ee916965f
Tsvetov <tsvetovpavel@gmail.com>
2014-08-14 12:25:37 +0400

    Update test.py
    Добавление новых функций

--- test.py
+++ test.py
@@ -6,4 +6,12 @@ sumrange_ctypes.restype = ctypes.c_ulonglong
 
 sumrange_ctypes.argtypes = ctypes.c_ulonglong,
 
-print sumrange_ctypes(10**8)
+
+def sum_range_cpython(arg)
+  sumrange_ctypes(10**8)
+
+def sum_range_python(arg):
+  return sum(xrange(arg))
+
+def gays_sum(arg):
+  return (arg*(arg+1))/2 - arg
